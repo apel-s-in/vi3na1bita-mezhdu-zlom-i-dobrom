492fe49d260e0ce125c22ca073623374510bd828
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2025-10-21 13:37:52 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// service-worker.js - v6.6.8
-const VERSION = '6.6.8';
+// service-worker.js - v6.6.9
+const VERSION = '6.6.9';
 const CACHE_NAME = `album-cache-v${VERSION}`;
 const OFFLINE_CACHE = 'album-offline-v1';
 
